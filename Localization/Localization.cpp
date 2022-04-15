e9aa2bdb8fc192561ec55c201079c18214ff4d44
Jeffrey Koopman <koopmajf@mail.uc.edu>
2022-04-15 13:25:54 -0400

    Localization

---
+++ Localization/Localization.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "Localization.h"
+
+string Localization::getCurrentLocale() {
+	return "DE";
+}
+
+void Localization::setCurrentLocale(string newLocale) {
+	locale = newLocale;
+}
+
+string Localization::translateString(string strToTranslate) {
+	strToTranslate = strToTranslate + " ,but now in German";
+	return strToTranslate;
+}
+
+Localization::Localization() {
+
+}
